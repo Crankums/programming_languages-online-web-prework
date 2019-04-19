08509c63a3e9f9aca3459a06a33bb1772d5f368e
Craig Donavin <crdonavin@gmail.com>
2019-04-19 00:46:04 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, lang_data|
     lang_data.each do |name, type_hash|
-      new_hash[name] ||= {:type => type_hash[:type]}
+      new_hash[name] ||= {:type => type_hash[:type], :style => [style]}
     end
   end
   binding.pry
